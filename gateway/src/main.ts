073e861d14d556967f1d12848017c47b9276c970
ambanawah <ambanawah.carlos@ictuniversity.edu.cm>
2026-06-06 09:40:03 +0100

    feat: add swagger API documentation update

--- gateway/src/main.ts
+++ gateway/src/main.ts
@@ -30,7 +30,7 @@ async function bootstrap() {
     .build();
 
     const document = SwaggerModule.createDocument(app, config);
-    SwaggerModule.setup('api-docs', app, document);
+    SwaggerModule.setup('api/docs', app, document);
 
   const port = process.env.PORT || 3000;
   await app.listen(port);
